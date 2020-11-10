f70d0042f1ffe327f66e442706fef95661fad685
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-10 21:03:59 +0200

    Vue & GraphQL

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,43 @@
+import ApolloClient from 'apollo-boost';
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import VueRouter from 'vue-router';
 import './bootstrap';
+import Post from './Post';
+import PostList from './PostList';
 
 window.Vue = Vue;
 Vue.use(VueRouter);
 
+const routes = [{
+        path: '/',
+        name: 'index',
+        component: PostList
+    },
+    {
+        path: '/post/:id',
+        name: 'post',
+        component: Post
+    }
+];
+
+Vue.use(VueApollo);
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'http://blog-ql.test/graphql'
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+});
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
 const app = new Vue({
     el: '#app',
+    apolloProvider,
+    router
 });
